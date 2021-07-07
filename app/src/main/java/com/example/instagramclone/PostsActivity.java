1cdfc03b86953c8742893b3951a6bed3e2d6e5c3
Alberto Cortés <albert.cortes.000@gmail.com>
2021-07-07 14:14:13 -0500

    User logout
    User can now logout of app clicking on the logout button of the PostsActivity

--- app/src/main/java/com/example/instagramclone/PostsActivity.java
+++ app/src/main/java/com/example/instagramclone/PostsActivity.java
@@ -2,13 +2,32 @@ package com.example.instagramclone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.parse.ParseUser;
 
 public class PostsActivity extends AppCompatActivity {
+
+    private Button btnLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_posts);
+
+        btnLogout = findViewById(R.id.btnLogout);
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ParseUser.logOut();
+                Intent i = new Intent(PostsActivity.this, LoginActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
     }
 }
